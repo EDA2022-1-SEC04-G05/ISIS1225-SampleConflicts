560be8cd33ef31aa8258d749aaf9931d43669e86
Borisan2001 <b.doval@uniandes.edu.co>
2022-02-04 12:13:47 -0500

    Cambios en main

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Modificación de Est-1 en el Lab 2
+    print("3- Cargar Book-Tags!!!...")
     print("0- Salir")
 
 
@@ -65,6 +65,7 @@ def loadBookTags():
     Cargar los Tags de libros
     """
     # TODO: Modificación de Est-1 en el Lab 2
+    print("Ultimo libro cargado: "+str(lt.lastElement(books)))
     pass
 
 
@@ -80,17 +81,15 @@ while True:
         print('Total de libros cargados: ' + str(lt.size(books)))
 
         # TODO: Modificación de Est-1 en el Lab 2
-
-
-        # TODO: Modificación de Est-2 en el Lab 2
-
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    elif True:
-        # TODO: Modificación de Est-2 en el Lab 2
-        pass
+        
+    elif int(inputs[0])==3:
+        print("Cargando información de Book-Tags...")
+        booktags = loadBookTags()
+        print("Total de Book-Tags cargados: "+str(lt.size(booktags)))
 
     else:
         sys.exit(0)
